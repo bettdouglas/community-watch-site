c9f993ff47e95b7361d2f26630d7daf6e927021c
Douglas Bett <bettdalpha@gmail.com>
2023-07-18 11:34:40 +0300

    fix link

--- community-watch-website/src/pages/index.js
+++ community-watch-website/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/about">
+            to="/docs/intro">
             Get Involved Now!
           </Link>
         </div>
